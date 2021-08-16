45a03df3638f6cadad3a9ed6aa04d1e1ffc362df
Wille <57924483+noxito@users.noreply.github.com>
2021-08-16 18:19:26 -0300

    Add files via upload

---
+++ asciiprinter/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+namespace asciiprinter
+{
+    class Program
+    {
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        private static extern IntPtr GetConsoleWindow();
+        private static IntPtr ThisConsole = GetConsoleWindow();
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+        private const int HIDE = 0;
+        private const int MAXIMIZE = 3;
+        private const int MINIMIZE = 6;
+        private const int RESTORE = 9;
+      
+        static void Main()
+        {
+            Console.SetWindowSize(Console.LargestWindowWidth, Console.LargestWindowHeight);
+            ShowWindow(ThisConsole, MAXIMIZE);
+            Console.ReadLine();
+
+            Console.WindowWidth = 170;
+            Console.WindowHeight = 44;
+            Image Picture = Image.FromFile("C:\\Users\\vitor\\Desktop\\asciiprinter\\test.png"); //Insert the path to the image. Example: C:\\Users\\Test\\Desktop\\test.png
+            Console.SetBufferSize((Picture.Width * 0x2), (Picture.Height * 0x2));
+            FrameDimension Dimension = new FrameDimension(Picture.FrameDimensionsList[0x0]);
+            int FrameCount = Picture.GetFrameCount(Dimension);
+            int Left = Console.WindowLeft, Top = Console.WindowTop;
+            char[] Chars = { '#', '#', '@', '%', '=', '+', '*', ':', '-', '.', ' ' };
+            Picture.SelectActiveFrame(Dimension, 0x0);
+            for (int i = 0x0; i < Picture.Height; i++)
+            {
+                for (int x = 0x0; x < Picture.Width; x++)
+                {
+                    Color Color = ((Bitmap)Picture).GetPixel(x, i);
+                    int Gray = (Color.R + Color.G + Color.B) / 0x3;
+                    int Index = (Gray * (Chars.Length - 0x1)) / 0xFF;
+                    Console.Write(Chars[Index]);
+                }
+                Console.Write('\n');
+                Thread.Sleep(50);
+            }
+            Console.SetCursorPosition(Left, Top);
+            Console.Read();
+        }
+    }
+}
